97071ab8c5e8489c4013b08c56996d9f4ac8e52f
Divinewill <51236443+divinoeze@users.noreply.github.com>
2020-09-03 14:25:54 +0100

    Database Setup

--- database/migrations/2020_09_03_112405_create_blogtags_table.php
+++ database/migrations/2020_09_03_112405_create_blogtags_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoriesTable extends Migration
+class CreateBlogtagsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('blogtags', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('categoryName');
-            $table->string('iconImage');
+            $table->integer('tag_id');
+            $table->integer('blog_id');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('blogtags');
     }
 }
--- database/migrations/2020_09_03_112424_create_blogcategories_table.php
+++ database/migrations/2020_09_03_112424_create_blogcategories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoriesTable extends Migration
+class CreateBlogcategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('blogcategories', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('categoryName');
-            $table->string('iconImage');
+            $table->integer('category_id');
+            $table->integer('blog_id');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('blogcategories');
     }
 }
